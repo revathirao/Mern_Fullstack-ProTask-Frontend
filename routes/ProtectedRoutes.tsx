270d6078255cd6f6307a6496451a7a015acc43f9
Revathi <revathi9605@gmail.com>
2026-01-30 18:24:55 -0500

    Added DashboardPage only with logout button with css

--- routes/ProtectedRoutes.tsx
+++ routes/ProtectedRoutes.tsx
@@ -1,6 +1,6 @@
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../src/context/authContext";
-import React, { useContext } from "react";
+import { useContext } from "react";
 import type { ReactNode } from "react";
 
 /**
--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,3 +1,27 @@
+import React, { useContext } from "react";
+import { AuthContext } from "../../context/authContext";
+/**
+ * Dashboard Component
+ * This page is accessible only by logged-in users.
+ * Displays a welcome message and a logout button.
+ */
 export default function Dashboard() {
-   return <h1>Welcome to Dashboard page</h1>; //To check the page initially
+   const { user, logout } = useContext(AuthContext); // Get current user and logout function from AuthContext
+
+   // return <h1>Welcome to Dashboard page</h1>; //To check the page initially
+   return (
+      <div className="dashboard-container">
+         {/* Centered card */}
+         <div className="dashboard-card">
+            {/* Welcome message for the user */}
+            <h1>Welcome, {user?.username || user?.email}!</h1>
+
+            {/* Logout button */}
+            <button onClick={logout}>
+               {" "}
+               // Calls logout function from AuthContext Logout
+            </button>
+         </div>
+      </div>
+   );
 }
