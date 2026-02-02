632f3c28aa9020fc7497f94db0873392affaaf81
Revathi <revathi9605@gmail.com>
2026-02-02 00:26:18 -0500

    added taskformprops interfacein types and created folder services

--- src/components/Tasks/TaskForm/TaskForm.tsx
+++ src/components/Tasks/TaskForm/TaskForm.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, type FormEvent } from "react";
 import axios from "axios";
 import type { TaskFormProps } from "../../../types";
+import ErrorMessage from "../../SharedComponents/ErrorHandler/ErrorHandler";
 
 /**
  * TaskForm Component
@@ -46,7 +47,57 @@ export default function TaskForm({
             />
          </div>
 
-         
+         {/*  Description field*/}
+         <div className="form-group">
+            <label htmlFor="Description">Description </label>
+            <textarea
+               id="description"
+               value={description} // controlled input using state
+               onChange={(e) => setDescription(e.target.value)} // update state on change
+               placeholder="Enter task description"
+            />
+         </div>
+
+         {/* Status Select */}
+         <div className="form-group">
+            <label htmlFor="status">Status</label>
+            <select
+               id="status"
+               value={status}
+               onChange={(e) => setStatus(e.target.value)}>
+               <option value="To Do">To Do</option>
+               <option value="In Progress">In Progress</option>
+               <option value="Done">Done</option>
+            </select>
+         </div>
+
+         {/* PrioritySelect */}
+         <div className="form-group">
+            <label htmlFor="priority">Priority</label>
+            <select
+               id="priority"
+               value={priority}
+               onChange={(e) => setPriority(e.target.value)}>
+               <option value="Low">Low</option>
+               <option value="Medium">Medium</option>
+               <option value="High">High</option>
+            </select>
+         </div>
+
+         {/* Display an inline error message if the 'error' state contains a value
+          *Shows validation errors (e.g., empty project name)*/}
+         {error && <ErrorMessage message={error} />}
+
+         {/* Form Acion Buttons */}
+
+         <div className="form-actions">
+            <button type="submit" disabled={isSubmitting}>
+               {task ? "Update Task" : "Create Task"}
+            </button>
+            <button type="button" onClick={onClose}>
+               Cancel
+            </button>
+         </div>
       </form>
    );
 }
